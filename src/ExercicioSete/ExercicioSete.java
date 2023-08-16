d4e8cccfcab20ee691d990ecf3f0c2a9b02f1a2a
Rodrigo Barbosa <rodbarbosa.dv@gmail.com>
2023-08-16 15:33:42 -0300

    Exercício Sete

--- src/ExercicioSete/ExercicioSete.java
+++ src/ExercicioSete/ExercicioSete.java
@@ -1,2 +1,28 @@
-package ExercicioSete;public class ExercicioSete {
+package ExercicioSete;
+import java.util.Scanner;
+public class ExercicioSete {
+
+    public static void main(String[] args) {
+
+        Scanner entrada = new Scanner(System.in);
+
+        // Leia uma temperatura em graus Celsius e apresentá-la convertida em graus Fahrenheit. Afórmula de
+        //conversão é:F=(9*C+160) / 5, sendo F a temperatura em Fahrenheit e C a temperatura emCelsius;
+
+        int celsius = 0;
+        float fahrenheit = 0.0f;
+
+        System.out.println("Digite uma temperatura em Celsius (C): ");
+         celsius = entrada.nextInt();
+
+         fahrenheit = (9*celsius+160)/5;
+        System.out.println("A temperatura digitada convertida para Fahrenheit é: " + fahrenheit);
+
+
+
+
+
+
+
+    }
 }
